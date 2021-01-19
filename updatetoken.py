0fa55a5a4f8d86421955eaa7c2c14c33f7b5683b
wangziyingwen <wz.lxh@outlook.com>
2021-01-19 19:52:46 +0800

    添加错误说明

--- updatetoken.py
+++ updatetoken.py
@@ -33,7 +33,7 @@ def getpublickey(Auth,geturl):
     return public_key
 
 #微软refresh_token获取
-def getmstoken(ms_token):
+def getmstoken(ms_token,appnum):
     headers={'Content-Type':'application/x-www-form-urlencoded'
             }
     data={'grant_type': 'refresh_token',
@@ -45,9 +45,9 @@ def getmstoken(ms_token):
     html = req.post('https://login.microsoftonline.com/common/oauth2/v2.0/token',data=data,headers=headers)
     jsontxt = json.loads(html.text)
     if 'refresh_token' in jsontxt:
-        print("微软密钥获取成功")
+        print(r'账号/应用 '+str(appnum)+' 的微软密钥获取成功')
     else:
-        print("微软密钥获取失败"+'\n'+"请检查secret里 CLIENT_ID , CLIENT_SECRET , MS_TOKEN 格式与内容是否正确，然后重新设置")
+        print(r'账号/应用 '+str(appnum)+' 的微软密钥获取失败'+'\n'+'请检查secret里 CLIENT_ID , CLIENT_SECRET , MS_TOKEN 格式与内容是否正确，然后重新设置')
     refresh_token = jsontxt['refresh_token']
     access_token = jsontxt['access_token']
     return refresh_token
@@ -61,11 +61,15 @@ def createsecret(public_key,secret_value):
     return b64encode(encrypted).decode("utf-8")
 
 #token上传
-def setsecret(encrypted_value,key_id,puturl):
+def setsecret(encrypted_value,key_id,puturl,appnum):
     headers={'Accept': 'application/vnd.github.v3+json','Authorization': Auth}
     #data={'encrypted_value': encrypted_value,'key_id': key_id}  ->400error
     data_str=r'{"encrypted_value":"'+encrypted_value+r'",'+r'"key_id":"'+key_id+r'"}'
     putstatus=req.put(puturl,headers=headers,data=data_str)
+    if putstatus.status_code >= 300:
+        print(r'账号/应用 '+str(appnum)+' 的微软密钥上传失败，请检查secret里 GH_TOKEN 格式与设置是否正确')
+    else:
+        print(r'账号/应用 '+str(appnum)+' 的微软密钥上传成功')
     return putstatus
     
 #调用 
@@ -75,12 +79,12 @@ for a in range(1, int(app_num)+1):
         client_secret=os.getenv('CLIENT_SECRET')
         ms_token=os.getenv('MS_TOKEN')
         puturl=r'https://api.github.com/repos/'+gh_repo+r'/actions/secrets/MS_TOKEN'
-        encrypted_value=createsecret(getpublickey(Auth,geturl),getmstoken(ms_token))
-        setsecret(encrypted_value,key_id,puturl)
+        encrypted_value=createsecret(getpublickey(Auth,geturl),getmstoken(ms_token,a))
+        setsecret(encrypted_value,key_id,puturl,a)
     else:
         client_id=os.getenv('CLIENT_ID_'+str(a))
         client_secret=os.getenv('CLIENT_SECRET_'+str(a))
         ms_token=os.getenv('MS_TOKEN_'+str(a))
         puturl=r'https://api.github.com/repos/'+gh_repo+r'/actions/secrets/MS_TOKEN_'+str(a)
-        encrypted_value=createsecret(getpublickey(Auth,geturl),getmstoken(ms_token))
-        setsecret(encrypted_value,key_id,puturl)
+        encrypted_value=createsecret(getpublickey(Auth,geturl),getmstoken(ms_token,a))
+        setsecret(encrypted_value,key_id,puturl,a)
